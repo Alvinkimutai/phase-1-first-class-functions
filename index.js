fe21127d16105dfd1292fffac95e6d2165662818
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-07-21 06:41:29 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function receivesAFunction(Alvin){
 }
 
 
-
+//returnsANamedFunction called Vscode
 function returnsANamedFunction(){
     return function Vscode(){};
 }
 
-
+//returnsAnAnonymousFunction
 function returnsAnAnonymousFunction(){
     return function(){};
 }
